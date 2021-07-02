352e71feed89bb5e238ecf3eb9820ac19b4f95be
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-07-01 21:08:37 -0300

    created Authenticate route

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import { UserController } from './app/controllers/UserController';
+import { AuthController } from './app/controllers/AuthController';
 
 const userController = new UserController();
+const authController = new AuthController();
 
 const routes = Router();
 
 routes.post('/users', userController.store);
-
+routes.post('/authenticate', authController.authenticate);
 
 export { routes };
